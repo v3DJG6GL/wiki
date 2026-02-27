20484e31b458c5836144896d42853bb5aadcf584
v3DJG6GL <72495210+v3DJG6GL@users.noreply.github.com>
2026-02-27 17:57:05 +0100

    add deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://v3DJG6GL.github.io',
+	base: '/wiki',
 	integrations: [
 		starlight({
 			title: 'My Docs',
